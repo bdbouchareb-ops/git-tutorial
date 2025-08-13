bd3d8f2629b40b74eed4072b2920aceed972157b
Douaa bouchareb <bd.bouchareb@ensta.edu.dz>
2025-08-13 16:27:49 +0200

    second new vertion

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 console.log('config.js');
-console.log('versionnew1');
+console.log('versionnew11');
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 console.log('index.js');
-console.log('versionnew1');
+console.log('versionnew11');
